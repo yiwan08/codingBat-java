1657e0ce9f04cc1bd2cdca8f5e402c9bac2e074a
yiwanSven <69824146+yiwanSven@users.noreply.github.com>
2022-05-12 13:45:25 -0400

    Create makePi.java

---
+++ array1/makePi.java
@@ -0,0 +1,18 @@
+/*
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+makePi() → [3, 1, 4]
+*/
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    System.out.println(makePi());
+  }
+
+  public static int[] makePi() {
+    int[] arrPi = {3, 1, 4};
+    return arrPi;
+  }
+}
