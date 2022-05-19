03137e22683e6222a1f60331bc17421995cceac7
yiwanSven <69824146+yiwanSven@users.noreply.github.com>
2022-05-19 13:32:59 -0400

    Create unlucky1.java

---
+++ array1/unlucky1.java
@@ -0,0 +1,27 @@
+/*
+We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1. Return true if the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.
+
+unlucky1([1, 3, 4, 5]) → true
+unlucky1([2, 1, 3, 4, 5]) → true
+unlucky1([1, 1, 1]) → false
+*/
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    int[] arr1 = {1, 2, 3, 4, 5};
+    System.out.println(unlucky1(arr1));
+  }
+
+  public static boolean unlucky1(int[] nums) {
+    int len = nums.length;
+    if(len < 2) {
+      return false;
+    }
+    if((nums[0] == 1 && nums[1] == 3) || (nums[1] == 1 && nums[2] == 3) || (nums[len - 2] == 1 && nums[len - 1] == 3)) {
+      return true;
+    }
+    return false;
+  }
+}
