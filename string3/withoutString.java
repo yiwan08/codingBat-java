3a6f5b5600e49d818bf89843b6583ced023335d8
yiwan08 <69824146+yiwan08@users.noreply.github.com>
2023-02-22 11:31:50 -0500

    Created withoutString.java

--- string3/withoutString.java
+++ string3/withoutString.java
@@ -2,7 +2,7 @@ public class Main
 {
   public static void main(String[] args)
   {
-    System.out.println(evenlySpaced(2, 2, 2));
+    System.out.println(withoutString("Hello there", "llo"));
   }
 
   public String withoutString(String base, String remove) {
