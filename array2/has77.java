fa029663195ebb85478f21eedb78a5e170c19eef
yiwan08 <69824146+yiwan08@users.noreply.github.com>
2023-04-25 11:22:02 -0400

    Update has77.java

--- array2/has77.java
+++ array2/has77.java
@@ -51,3 +51,19 @@ public boolean has77(int[] nums) {
   }
   return false;
 }
+
+// OR
+
+public boolena has77(int[] nums) {
+  for (int i = 0; i < nums.length; i++) {
+    if (i != nums.length - 2) {
+      if (nums[i] == 7 && nums[i+2] == 7) {
+        return true;
+      }
+    }
+    if (nums[i] == 7 && nums[i+2] == 7) {
+      return true;
+    }
+  }
+  return false;
+}
