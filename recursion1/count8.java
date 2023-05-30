eeed61e6d2e081ccf2ae823500ef7157f76b6e3d
yiwan08 <69824146+yiwan08@users.noreply.github.com>
2023-05-29 22:25:25 -0400

    Create count8.java

---
+++ recursion1/count8.java
@@ -0,0 +1,29 @@
+/*
+Given a non-negative int n,
+compute recursively (no loops) the count of the occurrences of 8 as a digit,
+except that an 8 with another 8 immediately to its left counts double, so 8818 yields 4.
+Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
+while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+count8(8) → 1
+count8(818) → 2
+count8(8818) → 4
+*/
+
+public class Main
+{
+  public static void main(String[] args) {
+  }
+  public int count8(int n) {
+    if (n == 0) {
+      return 0;
+    }
+    if (n % 10 == 8) {
+      if ((n / 10) % 10 == 8) {
+        return count8(n / 10) + 2;
+      }
+      return count8(n / 10) + 1;
+    }
+    return count8(n / 10);
+  }
+}
