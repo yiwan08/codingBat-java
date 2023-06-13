ef236411d0a69148462921a79a72f5179ba8ea00
yiwan08 <69824146+yiwan08@users.noreply.github.com>
2023-06-13 14:32:08 -0400

    Create countX.java

---
+++ recursion1/countX.java
@@ -0,0 +1,24 @@
+/*
+Given a string, compute recursively (no loops) the number of lowercase 'x' chars in the string.
+
+countX("xxhixx") → 4
+countX("xhixhix") → 3
+countX("hi") → 0
+*/
+
+public class Main
+{
+  public static void main(String[] args) {
+    System.out.pritnln(countX("xa"));
+  }
+  
+  public int countX(String str) {
+    if (str.length() == 0) {
+      return 0;
+    }
+    if (str.charAt(0) == 'x') {
+      return 1 + countX(str.substring(1));
+    }
+    return countX(str.substring(1));
+  }
+}
