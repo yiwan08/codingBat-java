a57bae946c097a621fdafa6fca3864d634d560ba
yiwan08 <69824146+yiwan08@users.noreply.github.com>
2024-05-28 14:36:44 -0400

    Create pairStar.java

---
+++ recursion1/pairStar.java
@@ -0,0 +1,24 @@
+/*
+Given a string, compute recursively a new string where identical chars that are adjacent in the original string are separated from each other by a "*".
+
+pairStar("hello") → "hel*lo"
+pairStar("xxyy") → "x*xy*y"
+pairStar("aaaa") → "a*a*a*a"
+*/
+
+public class Main
+{
+  public static void main(String[] args) {
+    System.out.println(pairStar("Lolllll"));
+  }
+
+  public String pairStar(String str) {
+    if (str.length() <= 1) {
+      return str;
+    }
+    if (str.charAt(0) == str.charAt(1)) {
+      return str.charAt(0) + "*" + pairStar(str.substring(1));
+    }
+    return str.charAt(0) + pairStar(str.substring(1));
+  }
+}
